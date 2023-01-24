74060bb22e2ddadede63a74c32194c6d24155522
Abdul Haseeb <62133331+askabdulhaseeb@users.noreply.github.com>
2023-01-24 17:39:32 +0500

    add colors

---
+++ lib/utilities/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  final Color darkGreen = const Color(0xFF00D18F);
+  final Color lightGreen = const Color(0xFF3EF984);
+  final Color darkPink = const Color(0xFFFE647B);
+  final Color darkOrange = const Color(0xFFFFAE5C);
+}
